fc9d2d78d7f417f61195912cb9be5103dde6ef81
Artem Shumidub <ashumidub@tradingview.com>
2019-11-19 09:06:00 +0300

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+h
+8app/src/main/java/com/shumidub/magicball/MainActivity.kt,2\4\24b395f01212261bd44e0e578773cfdfdfc0ea25
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+H
+app/google-services.json,f\d\fd8d34b142d02ec743a8efefefd2c097961666e9
---
+++ app/src/main/java/com/shumidub/magicball/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.shumidub.magicball
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        btn.setOnClickListener {
+            tv.text = if (Random.nextBoolean()) "Yes" else "No"
+        }
+    }
+
+}
